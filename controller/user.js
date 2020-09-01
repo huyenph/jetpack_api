20e479fc44c8c3d363aa1df1b5144abcfe5579c7
huyenpham <huyen.phamhoang96@gmail.com>
2020-09-01 17:28:09 +0700

    check existed email when update user - useFindAndModify

--- controller/user.js
+++ controller/user.js
@@ -11,8 +11,8 @@ const createUser = async (req, res) => {
       })
     }
     const { name, email, password, avatar, role } = req.body
-    const existedUser = await User.findOne({email})
-    if(existedUser) {
+    const existedUser = await User.findOne({ email })
+    if (existedUser) {
       return res.status(401).send({
         message: 'This email address is already being used!'
       })
@@ -67,7 +67,7 @@ const getUsers = async (req, res) => {
 const deleteUserById = async (req, res) => {
   try {
     const userId = req.params.id
-    const user = await User.findByIdAndDelete(userId)
+    const user = await User.findByIdAndDelete(userId, { useFindAndModify: false })
     if (!user) {
       return res.status(404).send()
     }
@@ -92,13 +92,23 @@ const updateUserById = async (req, res) => {
     const userId = req.params.id
     const allowUpdateFields = ['name', 'email', 'avatar']
     const updateFields = Object.keys(req.body)
+    const { email } = req.body
+    const existedUser = await User.findOne({ email })
+    if (existedUser._id != userId) {
+      return res.status(401).send({
+        message: 'This email address is already being used!'
+      })
+    }
     const canUpdate = updateFields.every(field => allowUpdateFields.includes(field))
     if (!canUpdate) {
       return res.status(400).send({
         message: 'Some field are not allowed to update!'
       })
     }
-    const user = await User.findByIdAndUpdate(userId)
+    const user = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+      useFindAndModify: false,
+    })
     if (!user) {
       return res.status(404).send()
     }

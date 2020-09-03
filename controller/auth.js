2e451d0a71f3170a6432bd616ab07fa674dbc9f7
huyenpham <huyen.phamhoang96@gmail.com>
2020-09-03 12:55:44 +0700

    nodemailer

--- controller/auth.js
+++ controller/auth.js
@@ -1,7 +1,25 @@
 const jwt = require('jsonwebtoken')
 const bcryptjs = require('bcryptjs')
+const nodemailer = require('nodemailer')
 const config = require('../config')
 const { User } = require('../model/user')
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '906ac091b0de48',
+    pass: 'b3de767059efba',
+  },
+})
+
+const message = {
+  from: 'elonmusk@tesla.com', // Sender address
+  to: 'huyen.phamhoang96@email.com',         // List of recipients
+  subject: 'Design Your Model S | Tesla', // Subject line
+  text: 'Have the most fun you can in a car. Get your Tesla today!',
+  html: '<h1>Have the most fun you can in a car!</h1><p>Get your <b>Tesla</b> today!</p>',
+}
 
 exports.login = async (req, res) => {
   console.log(req.headers.host)
@@ -31,6 +49,13 @@ exports.login = async (req, res) => {
     res.status(200).send({
       message: 'Success',
       token
+    })
+    transporter.sendMail(message, function (err, info) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(info)
+      }
     })
   } catch (error) {
     return res.status(500).send(error)
